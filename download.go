a17693488b9585032dab1e6d2e919eebe5ef9cd7
Giuseppe Manzo <manzo.giuseppe@gmail.com>
2023-08-07 14:58:04 +0200

    download stub

---
+++ download.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type AdcRequest http.Request
+
+func panic_if_error(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (req *AdcRequest) Prepare(rawurl string) *AdcRequest {
+	u, err := url.Parse(rawurl)
+	panic_if_error(err)
+
+	r, err := http.NewRequest("POST", rawurl, nil)
+	panic_if_error(err)
+
+	r.Header.Add("Accept", `text/html`)
+	r.Header.Add("Accept-Language", `it-IT,it;q=0.9`)
+	r.Header.Add("Cache-Control", `max-age=0`)
+	r.Header.Add("Connection", `keep-alive`)
+	r.Header.Add("Content-Type", `application/x-www-form-urlencoded`)
+	r.Header.Add("DNT", `1`)
+	r.Header.Add("Origin", u.Hostname())
+	r.Header.Add("Referer", rawurl)
+	r.Header.Add("Upgrade-Insecure-Request", `1`)
+	r.Header.Add("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36`)
+
+	return req
+}
+
+func (req *AdcRequest) SetDate(date string, reader io.Reader) *AdcRequest {
+
+	buffer, err := ioutil.ReadAll(reader)
+	panic_if_error(err)
+
+	//date 07%2F08%2F2023
+	buffer = append(buffer, `07%2F08%2F2023`...)
+
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(buffer))
+
+	return req
+}
