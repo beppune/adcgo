3136b797a594f2f0f32bf4e027ccbb6f73aba3ec
Giuseppe Manzo <manzo.giuseppe@gmail.com>
2023-08-07 21:43:29 +0200

    add viper and cobra

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"io/fs"
-	"io/ioutil"
-	"os"
-)
+import "github.com/beppune/adcgo/cmd"
 
 func main() {
-
-	os.Create("temp.txt")
-
-	//07%2F08%2F2023
-	b, _ := ioutil.ReadFile("body.txt")
-
-	b = append(b, `07%2F08%2F2023`...)
-	ioutil.WriteFile("temp.txt", b, fs.ModeAppend)
-
+	cmd.Execute()
 }
