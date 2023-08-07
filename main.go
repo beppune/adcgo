e08a520a82e1468bb76e3d368f543b6b39e32698
Giuseppe Manzo <manzo.giuseppe@gmail.com>
2023-08-07 15:13:38 +0200

    test copy and append

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"io/fs"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
-	
+
+	os.Create("temp.txt")
+
+	//07%2F08%2F2023
+	b, _ := ioutil.ReadFile("body.txt")
+
+	b = append(b, `07%2F08%2F2023`...)
+	ioutil.WriteFile("temp.txt", b, fs.ModeAppend)
+
 }
