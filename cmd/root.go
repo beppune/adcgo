975c2714afa3c78448ba74b39c41ba404a02d7c8
beppe <manzo.giuseppe@gmail.com>
2023-08-11 12:45:03 +0200

    Make config file optional
    If config file is not found adcgo run silently whit established default.
    It is possible to dump default file swith the root commad option --dump-config
    which will write the actual configuration into the .adcgo.yml file in the
    working directory

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -17,7 +18,15 @@ var rootCmd = &cobra.Command{
 	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+
+		dump, _ := cmd.PersistentFlags().GetBool("dump-config")
+		if dump {
+			fmt.Println("Dumping actual config into .adcgo.yml")
+			viper.WriteConfigAs(cfgFile)
+			os.Exit(0)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -36,6 +45,8 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is .adcgo.yml)")
 
+	rootCmd.PersistentFlags().Bool("dump-config", false, "Dump actuasl config in .adcgo.yml")
+
 	rootCmd.PersistentFlags().String("excel-path", `C:/Program Files/Microsoft Office/root/Office16/excel.exe`, "Fullpath to excel binary")
 	viper.BindPFlag("excel-path", rootCmd.PersistentFlags().Lookup("excel-path"))
 
@@ -49,12 +60,15 @@ func loadConfig() {
 		return
 	}
 
+	cfgFile = ".adcgo.yml"
+
 	d, _ := os.Getwd()
 	viper.AddConfigPath(d)
 	viper.SetConfigName(".adcgo")
 	viper.SetConfigType("yml")
 
-	if err := viper.ReadInConfig(); err != nil {
+	err := viper.ReadInConfig()
+	if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 		panic(err.Error())
 	}
 
